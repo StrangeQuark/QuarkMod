ee909036811a9aa31341073d33176b502f96f390
StrangeQuark <zacharyzirkle@hotmail.com>
2024-09-28 09:11:49 -0500

    Init

---
+++ src/main/java/com/strangequark/quarkmod/QuarkModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.strangequark.quarkmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class QuarkModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
